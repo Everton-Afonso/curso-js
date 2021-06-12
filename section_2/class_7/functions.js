ba7718d091142f0e1e466729609c568a8d70554c
Everton Otavio Afonso <evertonafonso0@gmail.com>
2021-06-11 22:45:04 -0300

    Functions(Básico)

---
+++ section_2/class_7/functions.js
@@ -0,0 +1,57 @@
+// function salutation(names) {
+//   return `Good Morning ${names}!`;
+// }
+
+// const value = salutation("Everton");
+// console.log(value);
+
+//*************************************************************
+
+// const names = [
+//   "Everton",
+//   "Elizandra",
+//   "Felipe",
+//   "Iara",
+//   "Roseli",
+//   "Eduarda",
+//   "Claudio",
+// ];
+
+// console.log(names);
+
+// names.forEach((name) => {
+//     console.log(name);
+// })
+
+//****************************************************************
+
+// function sum(x, y) {
+//   if (!x && !y) {
+//     console.log("Não foram enviados parametros");
+//   } else {
+//     const result = x + y;
+//     return result;
+//   }
+// }
+
+// console.log(sum(2, 5));
+
+//****************************************************************
+
+// const root = function(num) {
+//   return Math.sqrt(num);
+// };
+
+// console.log(root(9));
+// console.log(root(16));
+// console.log(root(25));
+
+//****************************************************************
+
+const arrowRoot = (num) => {
+  return Math.sqrt(num);
+};
+
+console.log(arrowRoot(9));
+console.log(arrowRoot(16));
+console.log(arrowRoot(25));
